376c9aaa25727a8c769000d5f946e0c8bd36727a
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2020-09-30 12:22:42 +0300

    feat: add solve of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return  array ? array.length ?  array.sort( (a, b) => a - b )[0]: 0 : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array ? array.length ?  array.sort( (a, b) => b - a )[0]: 0 : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array ? array.length ?  array.reduce( (sum, cur) => sum + cur, 0 ) / array.length : 0 : 0;
 }
